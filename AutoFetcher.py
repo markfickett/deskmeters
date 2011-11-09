1521cbdc847557a4e1e6861b4d05181b83ec2f93
Mark Fickett <mark.fickett@gmail.com>
2011-11-08 23:11:25 -0500

    Add utility class for periodic polling for updates/values.

---
+++ AutoFetcher.py
@@ -0,0 +1,39 @@
+__all__ = [
+	'LockGuard',
+	'AutoFetcher',
+]
+
+from Manifest import threading, time
+
+class LockGuard:
+	def __init__(self, lock):
+		self.__lock = lock
+	def __enter__(self):
+		self.__lock.acquire()
+	def __exit__(self, excClass, excObj, tb):
+		self.__lock.release()
+
+class AutoFetcher:
+	def __init__(self, interval):
+		self.__thread = threading.Thread(target=self.__updateForever)
+		self.__interval = max(0, float(interval))
+		self.__thread.daemon = True
+		self.__thread.start()
+		self.__lock = threading.Lock()
+
+	def _lockGuard(self):
+		return LockGuard(self.__lock)
+
+	def __updateForever(self):
+		while True:
+			time.sleep(self.__interval)
+			with self._lockGuard():
+				self._update()
+
+	def _update(self):
+		raise NotImplementedError()
+
+	def isAlive(self):
+		return self.__thread.isAlive()
+
+
